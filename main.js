27c0edf650dbc3e878bf643e5026e59acdc94109
Jennifer McKeithen <48393358+JenniferMcKeithen@users.noreply.github.com>
2019-11-06 14:23:22 -0600

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var app = new Vue({
     el: '#app', 
     data: {
         product: 'Socks',
-        description: 'Warm and cosy for your toesies!'
+        description: 'Warm and cosy for your toesies!',
+        image: href="https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg"
     }
 })
